c39c77c2d2eb7f2e0a49e092c38af046afb30dcf
Fajle Rabbi <frsbmaza2016@gmail.com>
2021-06-14 02:56:06 +0600

    Add files via upload
    This is without any final build assets.

---
+++ assets/js/index.js
@@ -0,0 +1,68 @@
+jQuery(document).ready(function($) {
+    $('#project-slider-init').owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+
+    $('#team-slider-init').owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+
+    $('#testimonial-slider-init').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+
+    $('.counter-number').counterUp({
+        delay: 10,
+        time: 1000
+    });
+    $('.toggle-nav').click(function() {
+        $('.header-nav').addClass('active');
+        $('html').css('overflow', 'hidden');
+    });
+    $('.close-nav').click(function() {
+        $('.header-nav').removeClass('active');
+        $('html').css('overflow', 'scroll');
+    })
+});
